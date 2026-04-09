6b76b3e13f964ff4de7535632eba82ff8ca3dced
FlyingSaturn <56539009+FlyingSaturn@users.noreply.github.com>
2026-04-10 04:56:52 +0530

    boilerplate

---
+++ heuristic.py
@@ -0,0 +1,24 @@
+cost_function = {
+        "cash_counter" : "1",
+        "biscuits" : "0",
+        "sanitary_napkins": "2i",
+        "chewing_gums" : "2i",
+        "chocolates" : "0",
+        "ice-cream" : "1.5 + i",
+        "chips" : "3",
+        "cold_drinks": "5 + 5i"
+}
+
+def shopkeeper(name):
+     // Determine which item to choose based on cost  
+    // Traverse
+    // Come back
+
+
+//  cost = cost_function.get(name, "Unknown")
+
+
+def customer():
+    // Generate based on queues
+    // ask for random items in random quantities or 1
+    // 
